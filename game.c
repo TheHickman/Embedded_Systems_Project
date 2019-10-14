5c75a4b935072da377822d1045efb42f8b7bae71
Taran Jennison <tmj49@uclive.ac.nz>
2019-10-15 12:32:27 +1300

    Scoring and speed changes now work

--- game.c
+++ game.c
@@ -16,38 +16,57 @@
 #include "ir_uart.h"
 #include "display.h"
 #include "draw.h"
+#include "string.h"
+#include "stdlib.h"
 
-#define DISP_HZ 500
+#define DISP_HZ 600
 #define GAME_SIZE 20
 
 typedef struct important {
     char arrow;
     int randomIndex;
-    uint8_t numarrows;
     int score;
+    uint16_t time_gap;
+    uint16_t total_time_loop;
 } game_info;
 
-
-int get_score(int count)
+int check_input(game_info* game)
 {
+    navswitch_update();
+    int out = 0;
+    if (navswitch_push_event_p (NAVSWITCH_NORTH) && game->arrow == 'U') {
+        out = 1;
+    } else if (navswitch_push_event_p (NAVSWITCH_SOUTH) && game->arrow == 'D') {
+        out = 1;
+    } else if (navswitch_push_event_p (NAVSWITCH_EAST) && game->arrow == 'R') {
+        out = 1;
+    } else if (navswitch_push_event_p (NAVSWITCH_WEST) && game->arrow == 'L') {
+        out = 1;
+    }
+    return out;
+}
+
+int get_score(uint16_t count, game_info* game)
+{
+    int gap = game->time_gap;
     int score;
-    int pre_score = count - 500;
-    if (pre_score <= -250) {
+    int pre_score = count - 2*gap;
+    if (pre_score <= -gap) {
         score = 0;
     } else if (pre_score < 0) {
-        score = pre_score + 250;
+        score = pre_score + gap;
     } else {
-        score = 250 - pre_score;
+        score = gap - pre_score; //scale score by gap so scores are consistant across speeds?
     }
+
     return score;
 }
 
 void switch_arrow(game_info* game)
 {
     char arrows[4] = {'R', 'L', 'D', 'U'};
-	timer_tick_t timer = timer_get();
+    timer_tick_t timer = timer_get();
     game->randomIndex = (game->randomIndex + (timer * 11245 + 12345)) % 4;
-    game->numarrows += 1;
     game->arrow = arrows[game->randomIndex];
 }
 
@@ -69,6 +88,7 @@ int main (void)
         pacer_wait();
         tinygl_update ();
         navswitch_update();
+
         if (navswitch_push_event_p (NAVSWITCH_NORTH)) {
             speed += 1;
             if (speed > '5') { //Looping through 1-5
@@ -94,19 +114,24 @@ int main (void)
         }
 
     }
-
     tinygl_clear();
-    game_info game = {'U', 0, 45, 0};
+
+    //Init stuff for game
+    game_info game = {'U', 0, 0, 0, 0};
     uint16_t counter = 0;
+    game.time_gap = 75 * (6-(speed-49));
+    game.total_time_loop = 3 * game.time_gap;
     int task = 0;
     uint8_t how_many_arrows = 0;
 
+
+    //main gameplay loop (maybe seperate to somewhere else?)
     while (how_many_arrows < 15) {
 
         pacer_wait();
-        counter = (counter + 1) % 750;
+        counter = (counter + 1) % game.total_time_loop;
 
-        if (counter%250 == 0) {
+        if (counter%game.time_gap == 0) {
             task = (task + 1) % 3;
         }
 
@@ -119,10 +144,32 @@ int main (void)
             display_arrow(game.arrow, 0);
         } else if (task == 1) { // second arrow
             display_arrow(game.arrow, 1);
+            if (check_input(&game)) {
+                game.score += get_score(counter, &game);
+            }
         } else if (task == 2) { // input
             display_arrow(game.arrow, 2);
+            if (check_input(&game)) {
+                game.score += get_score(counter, &game);
+            }
         }
 
     }
-    display_mess ("Winner");
+
+    //Display score
+    if (game.score == 0) {
+        display_mess ("zero");
+    } else {
+        char sNum[10];
+        itoa(game.score, sNum, 10);
+        sNum[9] = '\0';
+        display_mess (sNum);
+        tinygl_clear();
+    }
+
+    //Comunicate score
+
+    //Get winner
+
+    //Display result
 }
