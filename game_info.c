2b4a9883e753f290f702ef87ba5829a7e61e9fca
Taran Jennison <tmj49@uclive.ac.nz>
2019-10-18 12:22:54 +1300

    lol, never mind....

--- game_info.c
+++ game_info.c
@@ -121,7 +121,6 @@ void select_speed(game_info* game)
     //set time gap and total time loop
     game->time_gap = 75 * (7-(game->speed));
     game->total_time_loop = 3 * game->time_gap;
-    game->randomIndex = counter;
 }
 
 
